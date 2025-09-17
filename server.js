5316caf5acbe490b3c9e8fe0319eea550d543d81
Priyanshu <mishrapriyanshu026@gmail.com>
2025-09-17 21:38:31 +0530

    changed import name

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
 import connectDB from "./config/db.js";
-import Room from "./models/room.js";
+import Room from "./models/Room.js";
 import dotenv from "dotenv";
 
 dotenv.config(); // load .env
@@ -58,7 +58,12 @@ io.on("connection", (socket) => {
     });
 });
 
+app.get("/", (req, res) => {
+    res.send("CodeX Backend  is running...");
+});
+
 const PORT = process.env.PORT || 5000;
+
 
 server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
